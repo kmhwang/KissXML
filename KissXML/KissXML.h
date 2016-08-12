43325882a7760eca57622f8e850c50f0f7831c6d
Ken M. Hwang <ken.m.hwang@gmail.com>
2016-08-12 10:19:51 +0800

    Simple Xcode Project For Carthage Support

--- KissXML/KissXML.h
+++ KissXML/KissXML.h
@@ -1,12 +1,26 @@
-@import Foundation;
+//
+//  KissXML.h
+//  KissXML
+//
+//  Created by Ken M. Hwang on 8/12/16.
+//  Copyright © 2016 Ken M. Hwang. All rights reserved.
+//
 
-#import "DDXMLElementAdditions.h"
-#import "NSString+DDXML.h"
-#import "DDXML.h"
-#import "DDXMLDocument.h"
-#import "DDXMLElement.h"
-#import "DDXMLNode.h"
+#import <Foundation/Foundation.h>
 
+//! Project version number for KissXML.
 FOUNDATION_EXPORT double KissXMLVersionNumber;
+
+//! Project version string for KissXML.
 FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
 
+// In this header, you should import all the public headers of your framework using statements like #import <KissXML/PublicHeader.h>
+
+@import Foundation;
+
+#import <KissXML/DDXMLElementAdditions.h>
+#import <KissXML/NSString+DDXML.h>
+#import <KissXML/DDXML.h>
+#import <KissXML/DDXMLDocument.h>
+#import <KissXML/DDXMLElement.h>
+#import <KissXML/DDXMLNode.h>
